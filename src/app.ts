82f395cd7247cd9908546adf456f8990218b27c7
iamrazzakk <rakibt23p@gmail.com>
2025-06-14 20:08:49 +0600

    feat(dependencies): add express-rate-limit and update package resolutions
    - Introduced express-rate-limit to enhance rate limiting capabilities in the application.
    - Updated yarn.lock to reflect changes in package resolutions, switching from yarnpkg to npm registry for various dependencies.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import session from "express-session";
 import passport from "./config/passport";
 import handleStripeWebhook from "./helpers/handleStripeWebhook";
+import { ddosProtection } from "./app/middlewares/ddosProtection";
 const app = express();
 app.post(
   "/api/stripe/webhook",
@@ -22,6 +23,7 @@ app.use(Morgan.errorHandler);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(ddosProtection)
 
 //file retrieve
 app.use(express.static("uploads"));
