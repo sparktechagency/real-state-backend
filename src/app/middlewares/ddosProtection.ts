9f6c835919803fc6cbcaeacab87a5f8a9793bcef
iamrazzakk <rakibt23p@gmail.com>
2025-06-14 20:44:47 +0600

    feat(ddosProtection): update rate limiting configuration
    - Commented out the default windowMs setting and increased the max requests limit to 1 billion, allowing for more extensive request handling while maintaining protection against DDoS attacks.

--- src/app/middlewares/ddosProtection.ts
+++ src/app/middlewares/ddosProtection.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const ddosProtection = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
+    // windowMs: 15 * 60 * 1000,
+    max: 1000000000,
     message: {
         success: false,
         message: "Too many requests from this IP, please try again after 15 minutes",
