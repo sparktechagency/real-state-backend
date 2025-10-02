9a4d5ff0ac6925f8f3f44397be40b8e0b96786e1
iamrazzakk <rakibt23p@gmail.com>
2025-10-02 16:40:46 +0600

    add new feature

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,8 +1,9 @@
+import { USER_ROLES } from "../../../enums/user";
 import QueryBuilder from "../../builder/QueryBuilder"
 import { User } from "../user/user.model"
 
 const getAllUsersFromDB = async (query: Record<string, any>) => {
-    const result = new QueryBuilder(User.find({ verified: true }), query)
+    const result = new QueryBuilder(User.find({ verified: true, role: USER_ROLES.AGENCY }), query)
         .filter()
         .sort()
         .paginate();
