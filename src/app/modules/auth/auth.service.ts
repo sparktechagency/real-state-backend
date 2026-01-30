96904c88fd7f141d9ac783cedca643fa04082be4
iamrazzakk <rakibt23p@gmail.com>
2026-01-30 12:48:02 +0600

    refactor: remove sensitive logging from loginUserFromDB

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -20,10 +20,6 @@ import { USER_ROLES } from "../../../enums/user";
 
 //login
 const loginUserFromDB = async (payload: ILoginData) => {
-  console.log("email:", payload.email);
-  console.log("password:", payload.password);
-  console.log("deviceId:", payload.deviceId);
-  console.log("deviceToken:", payload.deviceToken);
   const { email, password, deviceId, deviceToken } = payload;
 
   const isExistUser: any = await User.findOne({ email })
